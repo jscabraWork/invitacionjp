40eac5f3fe9145fdfac391811066f6cab15fe39d
Juan Sebastian Cabra Leon <js.cabra@uniandes.edu.co>
2023-09-14 22:17:11 -0500

    generar componente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { InformacionComponent } from './informacion/informacion.component';
+import { DesscodeComponent } from './desscode/desscode.component';
+import { ConfirmarComponent } from './confirmar/confirmar.component';
+import { VideoComponent } from './video/video.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    InformacionComponent,
+    DesscodeComponent,
+    ConfirmarComponent,
+    VideoComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/desscode/desscode.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-desscode',
+  templateUrl: './desscode.component.html',
+  styleUrls: ['./desscode.component.css']
+})
+export class DesscodeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
